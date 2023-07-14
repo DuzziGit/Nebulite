13eb8a3858b3f2248731088a8790c47ed08c1127
DuzziGit <danteduzzi@live.ca>
2023-07-14 15:19:35 -0400

    Character animation controller handling remade

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,10 +4,20 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    const string idleUp = "idleAnimUp";
+    const string idleDown = "idleAnimDown";
+    const string idleRight = "idleAnimRight";
+    const string idleLeft = "idleAnimLeft";
+
+    const string walkUp = "WalkUp";
+    const string walkDown = "WalkDown";
+    const string walkRight = "WalkSideRight";
+    const string walkLeft = "WalkSideLeft";
+
     public float moveSpeed;
     public Rigidbody2D rb2d;
     private Vector2 moveInput;
-
+    private int direction;
     public LayerMask materialLayer;
     public float raycastDistance = 5f;
     public float raycastLineWidth = 0.1f;
@@ -37,13 +47,14 @@ public class PlayerMovement : MonoBehaviour
         moveInput.x = Input.GetAxisRaw("Horizontal");
         moveInput.y = Input.GetAxisRaw("Vertical");
         moveInput.Normalize();
-
+        handleAnimation();
         rb2d.velocity = moveInput * moveSpeed;
 
         // Update animator parameters
         animator.SetFloat("MoveX", moveInput.x);
         animator.SetFloat("MoveY", moveInput.y);
         animator.SetBool("IsWalking", moveInput.magnitude > 0);
+
         if (moveInput.magnitude > 0)
         {
             animator.SetBool("isWalking", true);
@@ -52,12 +63,55 @@ public class PlayerMovement : MonoBehaviour
             Debug.Log(moveInput.x);
             Debug.Log(moveInput.y);
 
-        } else if (moveInput.magnitude <= 0)
+            // Calculate the angle of the movement direction
+            float angle = Mathf.Atan2(moveInput.y, moveInput.x) * Mathf.Rad2Deg;
+
+            // Set the corresponding animator parameter based on the angle
+            if (angle >= -22.5f && angle < 22.5f)
+            {
+                // Right
+                direction = 0;
+            }
+            else if (angle >= 22.5f && angle < 67.5f)
+            {
+                // Up Right
+                direction = 1;
+            }
+            else if (angle >= 67.5f && angle < 112.5f)
+            {
+                // Up
+                direction = 2;
+            }
+            else if (angle >= 112.5f && angle < 157.5f)
+            {
+                // Up Left
+                direction = 3;
+            }
+            else if (angle >= 157.5f || angle < -157.5f)
+            {
+                // Left
+                direction = 4;
+            }
+            else if (angle >= -157.5f && angle < -112.5f)
+            {
+                // Down Left
+                direction = 5;
+            }
+            else if (angle >= -112.5f && angle < -67.5f)
+            {
+                // Down
+                direction = 6;
+            }
+            else if (angle >= -67.5f && angle < -22.5f)
+            {
+                // Down Right
+                direction = 7;
+            }
+        }
+        else
         {
             animator.SetBool("isWalking", false);
-
         }
-
 
         RaycastHit2D hit = Physics2D.Raycast(lastPosition, lastMoveDirection, raycastDistance, materialLayer);
         Debug.DrawRay(lastPosition, lastMoveDirection * raycastDistance, Color.green);
@@ -85,6 +139,7 @@ public class PlayerMovement : MonoBehaviour
             Debug.Log("Material: " + material.Key + ", Amount: " + material.Value);
         }
     }
+
     public void AddCoins(int amount)
     {
         coins += amount;
@@ -99,9 +154,47 @@ public class PlayerMovement : MonoBehaviour
         materials[type] += amount;
     }
 
+    void handleAnimation()
+    {
+        if (direction == 0)
+        {
+            animator.Play(moveInput.magnitude > 0 ? walkRight : idleRight);
+        }
+        else if (direction == 1)
+        {
+            animator.Play(moveInput.magnitude > 0 ? walkUp : idleUp);
+        }
+        else if (direction == 2)
+        {
+            animator.Play(moveInput.magnitude > 0 ? walkUp : idleUp);
+        }
+        else if (direction == 3)
+        {
+            animator.Play(moveInput.magnitude > 0 ? walkUp : idleUp);
+        }
+        else if (direction == 4)
+        {
+            animator.Play(moveInput.magnitude > 0 ? walkLeft : idleLeft);
+        }
+        else if (direction == 5)
+        {
+            animator.Play(moveInput.magnitude > 0 ? walkDown : idleDown);
+        }
+        else if (direction == 6)
+        {
+            animator.Play(moveInput.magnitude > 0 ? walkDown : idleDown);
+        }
+        else if (direction == 7)
+        {
+            animator.Play(moveInput.magnitude > 0 ? walkDown : idleDown);
+        }
+        else
+        {
+            animator.Play(idleUp);
+        }
+    }
 
-
-void UpdateLineRenderer()
+    void UpdateLineRenderer()
     {
         if (lineRenderer != null)
         {
