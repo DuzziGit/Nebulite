0f1ece590b9a072561d7c031dad1c77157ed7118
DuzziGit <danteduzzi@live.ca>
2023-07-14 04:33:39 -0400

    Add a tether from the ship to the player

---
+++ Assets/CharacterController.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    public Transform shipTransform; // Reference to the ship's transform
+    private LineRenderer lineRenderer; // Reference to the LineRenderer component
+
+    private void Start()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+        lineRenderer.startWidth = 0.05f;
+        lineRenderer.endWidth = 0.05f;
+    }
+
+    private void Update()
+    {
+        // Calculate the position for the line renderer at the character's belly
+        Vector3 bellyPosition = transform.position + new Vector3(0f, -0.5f, 0f);
+
+        // Update the positions of the line renderer
+        lineRenderer.SetPosition(0, bellyPosition);
+        lineRenderer.SetPosition(1, shipTransform.position);
+    }
+}
